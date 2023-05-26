41e433ec14bf37cb8c55dc32267d2676f048a4cf
saifion33 <mxofficialplayer@gmail.com>
2023-05-26 10:54:38 +0530

    initial commit - setup Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+function App() {
+
+  return (
+    <div className='text-2xl text-blue-600 font-semibold text-center py-4'>
+      This is Twitter clone
+    </div>
+  )
+}
+
+export default App
